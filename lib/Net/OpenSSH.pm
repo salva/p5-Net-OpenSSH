1f7b5563ad8806fd3e8077e4d7d9a1bf6e68400a
Salvador <sfandino@yahoo.com>
2011-11-24 19:00:49 +0100

    remove useless backslashes from regular expression

--- lib/Net/OpenSSH.pm
+++ lib/Net/OpenSSH.pm
@@ -954,8 +954,8 @@ sub _load_module {
     1
 }
 
-my $noquote_class = '\.\\w/\\-=@';
-my $glob_class    = '*?\\[\\],{}:!.^~';
+my $noquote_class = '.\\w/\\-=@';
+my $glob_class    = '*?\\[\\],{}:!^~';
 
 sub _arg_quoter {
     sub {
