222a37c96cd0d0028f5f1e080b408823e7905cd0
Salvador <sfandino@yahoo.com>
2013-03-21 19:17:19 +0100

    add aliases for shell styles

--- lib/Net/OpenSSH/ShellQuoter.pm
+++ lib/Net/OpenSSH/ShellQuoter.pm
@@ -6,9 +6,15 @@ use Carp;
 
 use Net::OpenSSH::ModuleLoader;
 
+my %alias = (bash => 'POSIX',
+             sh   => 'POSIX',
+             ksh  => 'POSIX',
+             tcsh => 'csh');
+
 sub quoter {
     my ($class, $style) = @_;
     $style = 'POSIX' unless defined $style;
+    $style = $alias{$style} if defined $alias{$style};
     $style =~ /^\w+$/ or croak "bad quoting style $style";
 
     my $impl = "Net::OpenSSH::ShellQuoter::$style";
