bbd5b544f156f04e5acfe5e564d4f0f021c28a6d
Salvador Fandino <sfandino@yahoo.com>
2013-06-28 17:26:46 +0200

    forward require error on _load_module; prepare for 0.61_04 release

--- lib/Net/OpenSSH/ModuleLoader.pm
+++ lib/Net/OpenSSH/ModuleLoader.pm
@@ -12,15 +12,19 @@ our @EXPORT = qw(_load_module);
 sub _load_module {
     my ($module, $version) = @_;
     $loaded_module{$module} ||= do {
-	do {
-	    local ($@, $SIG{__DIE__});
-	    eval "require $module; 1"
-	} or croak "unable to load Perl module $module";
-        1
+        my $err;
+        do {
+            local ($@, $SIG{__DIE__});
+            my $ok = eval "require $module; 1";
+            $err = $@;
+            $ok;
+        } or croak "unable to load Perl module $module: $err";
     };
     if (defined $version) {
-	local ($@, $SIG{__DIE__});
-	my $mv = eval "\$${module}::VERSION" || 0;
+        my $mv = do {
+            local ($@, $SIG{__DIE__});
+            eval "\$${module}::VERSION";
+        } || 0;
 	(my $mv1 = $mv) =~ s/_\d*$//;
 	croak "$module version $version required, $mv is available"
 	    if $mv1 < $version;
