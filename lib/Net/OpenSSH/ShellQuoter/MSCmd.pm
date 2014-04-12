19493aad0700ad973ae8936a19821c4d0b642a0d
Salvador Fandino <sfandino@yahoo.com>
2014-04-12 12:51:18 +0200

    document windows quoters, prepare for release

--- lib/Net/OpenSSH/ShellQuoter/MSCmd.pm
+++ lib/Net/OpenSSH/ShellQuoter/MSCmd.pm
@@ -31,3 +31,39 @@ sub shell_fragments {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Net::OpenSSH::ShellQuoter::MSCmd - Quoter for Windows cmd.exe
+
+=head1 DESCRIPTION
+
+This quoter is intended for interaction with SSH servers runnning on
+Windows which invoke the requested commands through the C<cmd.exe> shell.
+
+Because of C<cmd.exe> not doing willcard expansion (on Windows this
+task is left to the final command), glob quoting just quotes
+everything.
+
+Some Windows servers use C<Win32::CreateProcess> to run the C<cmd.exe>
+shell which runs the requested comand. In that case, both the C<MSCmd>
+and C<MSWin> quoters have to be chained (and BTW, order matters):
+
+   $ssh = Net::OpenSSH->new(...,
+                            remote_shell => 'MSCmd,MSWin');
+
+Actually, C<cmd.exe> may require not quoting at all when the requested
+command is a builtin (for instance, C<echo>).
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008-2014 by Salvador FandiE<ntilde>o
+(sfandino@yahoo.com)
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
--- lib/Net/OpenSSH/ShellQuoter/MSWin.pm
+++ lib/Net/OpenSSH/ShellQuoter/MSWin.pm
@@ -25,3 +25,29 @@ sub quote {
 sub shell_fragments { wantarray ? () : '' }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Net::OpenSSH::ShellQuoter::MSWin - Quoter for Win32::CreateProcess
+
+=head1 DESCRIPTION
+
+This quoter is intended for interaction with SSH servers runnning on
+Windows which use the C<Win32::CreateProcess> system call to launch the
+requested command.
+
+Because of C<Win32::CreateProcess> not doing willcard expansion, glob
+quoting just quotes everything.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008-2014 by Salvador FandiE<ntilde>o
+(sfandino@yahoo.com)
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
