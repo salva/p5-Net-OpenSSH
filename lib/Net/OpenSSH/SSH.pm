e339c80feda8560cd8a33b50a11c16eb0b7e3a07
Salvador Fandino <sfandino@yahoo.com>
2014-04-12 12:51:38 +0200

    add dummy package Net::OpenSSH::SSH

---
+++ lib/Net/OpenSSH/SSH.pm
@@ -0,0 +1,19 @@
+package Net::OpenSSH::SSH;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::OpenSSH::SSH - Perl SSH client package implemented on top of OpenSSH
+
+=head1 DESCRIPTION
+
+Use the real thing: L<Net::OpenSSH>.
+
+This namespace is used so that the module gets indexed under the
+C<SSH> tag on popular CPAN search engines such as
+L<http://metacpan.org> and L<http://search.cpan.org>.
+
+=cut
