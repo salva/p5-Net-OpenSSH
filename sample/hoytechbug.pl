b8bcbecdce6b5fa60fed7462471e155542fe2244
Salvador Fandiño <sfandino@yahoo.com>
2016-02-05 13:48:10 +0100

    improve hoytechbug script

--- sample/hoytechbug.pl
+++ sample/hoytechbug.pl
@@ -5,16 +5,21 @@ use warnings;
 
 use Net::OpenSSH;
 
-my $ssh = Net::OpenSSH->new('localhost');
-$ssh->die_on_error;
+my $host = shift // 'localhost';
 
-my @pid;
-for (0..10) {
-    push @pid, $ssh->spawn({tty => 1}, 'id >/dev/null');
-    $ssh->die_on_error;
+for my $count (0..1e6) {
+    my $ssh = Net::OpenSSH->new($host);
+    next if $ssh->error;
+
+    for my $mid (0..5) {
+        my @pid;
+        for (0..5) {
+            push @pid, $ssh->spawn({tty => 1}, 'id >/dev/null');
+            $ssh->die_on_error;
+        }
+        waitpid $_, 0 for grep defined, @pid;
+        print "[$count/$mid] ok!\r\n";
+    }
 }
 
-waitpid $_, 0 for grep defined, @pid;
-print "ok!\r\n";
-sleep 1;
 
